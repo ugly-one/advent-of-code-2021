1d87f10939d7b36f9402d0a8ca9fb0659b889e59
UglyOne <tomasz@gnatonski.com>
2022-09-19 18:27:31 +0200

    Day 9 part 1 done

---
+++ Program.fs
@@ -0,0 +1,7 @@
+﻿open Day9
+
+let arrayInput = Array.map (fun i -> {Value = i; IsMinimum = No}) input.Values
+let result = calculateLocalMinima arrayInput
+result |> List.iter printPoint
+let values = List.map (fun p -> p.Value) result
+printfn "%d" (List.sum values + List.length values)
