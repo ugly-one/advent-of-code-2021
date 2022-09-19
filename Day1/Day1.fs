871abc1d08eb7dc567f2da61361adf5c746d6e3d
UglyOne <tomasz@gnatonski.com>
2022-09-19 22:19:34 +0200

    day 1 part 2 done

--- Day1/Day1.fs
+++ Day1/Day1.fs
@@ -8,3 +8,20 @@ let calculate input =
 
     let (a, _) = Array.foldBack folder input (0, None)
     a
+
+let calculateWindow input = 
+    let folder (item: int) (result, w1, w2) = 
+        if (Array.length w1 = 0) then 
+            let newW1 = Array.append [|item|] w1
+            (result, newW1, w2)
+        elif (Array.length w1 = 1 || Array.length w1 = 2) then 
+            let newW1 = Array.append [|item|] w1
+            let newW2 = Array.append [|item|] w2
+            (result, newW1, newW2)
+        else 
+            let newW2 = Array.append [|item|] w2
+            // take 2 first items from newW2 and place it as w1
+            if (Array.sum w1 > Array.sum newW2) then (result + 1, newW2, newW2[..1]) else (result, newW2, newW2[..1])
+         
+    let (a, _, _) = Array.foldBack folder input (0, [||], [||])
+    a
--- Program.fs
+++ Program.fs
@@ -2,4 +2,4 @@
 open Day1
 open Day1_input
 
-calculate input |>  printfn "%d"
+calculateWindow input |>  printfn "%d"
